982c74e995bb78400633cdf4a302d6dc808d1b45
M S <mo.sa963@hotmail.com>
2023-08-20 21:23:40 +0300

    Add types

---
+++ lib/constants/types.dart
@@ -0,0 +1,7 @@
+import 'package:task/models/Type.dart';
+
+  final List<Type> types = [
+    Type(id: 1, name: "التصنيف 1"),
+    Type(id: 2, name: "التصنيف 2"),
+    Type(id: 3, name: "التصنيف 3"),
+  ];
