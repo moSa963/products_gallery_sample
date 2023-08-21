9e18dc79bd0ecf2d70e22607d2110d7b5d9b4f8f
M S <mo.sa963@hotmail.com>
2023-08-21 21:02:34 +0300

    Add ProductsProvider and colors to the theme and flip the direction to be rtl

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:task/pages/home_page.dart';
+import 'package:task/providers/products_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider<ProductsProvider>(
+    create: (_) => ProductsProvider(),
+    child:const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,9 +18,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter task',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 62, 184, 111),
+          background: const Color.fromARGB(255, 245, 245, 245),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 245, 245, 245),
+        ),
         useMaterial3: true,
       ),
+      builder: (context, child) {
+        return Directionality(
+          textDirection: TextDirection.rtl,
+          child: child!,
+        );
+      },
       home: const HomePage(),
     );
   }
