15de6d4fdcba67e511a2e45afc6cf96f692b4108
M S <mo.sa963@hotmail.com>
2023-08-22 23:12:00 +0300

    Add load method to ProductsProvider

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,12 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:task/models/product.dart';
+import 'package:task/models/product_image.dart';
+import 'package:task/services/database_service.dart';
 
 class ProductsProvider with ChangeNotifier {
-  final List<Product> _products = [];
+  List<Product> _products = [];
   List<Product> get products => _products;
-  
+  final _service = DatabaseService();
 
-  void add(Product product) {
+  ProductsProvider() {
+    load(null);
+  }
+
+  void load(int? type) async {
+    _products = await _service.select(type);
+    notifyListeners();
+  }
+
+  Future<void> add(Product product, List<File> images) async {
+    for (final image in images) {
+      product.images.add(ProductImage(
+        data: base64Encode(image.readAsBytesSync()),
+      ));
+    }
+
+    await _service.insert(product);
     _products.add(product);
     notifyListeners();
   }
