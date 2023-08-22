15f3562eeaac5ace6602b12aa0c463853241d464
M S <mo.sa963@hotmail.com>
2023-08-22 23:09:08 +0300

    Add toMap method and fromMap factory to Product model

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,5 +1,6 @@
-import 'package:task/models/product_Image.dart';
-import 'package:task/models/Type.dart';
+import 'package:task/constants/types.dart';
+import 'package:task/models/product_image.dart';
+import 'package:task/models/type.dart';
 
 class Product {
   int id;
@@ -10,10 +11,30 @@ class Product {
   List<ProductImage> images;
 
   Product(
-      {this.id=0,
-      this.name="",
-      this.shop="",
-      this.price=0,
+      {this.id = 0,
+      this.name = "",
+      this.shop = "",
+      this.price = 0,
       required this.images,
       required this.type});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'shop': shop,
+      'price': price,
+      'type': type.id,
+    };
+  }
+
+  factory Product.fromMap(Map<String, Object?> map) {
+    return Product(
+      id: map["id"] as int,
+      name: map["name"] as String,
+      shop: map["shop"] as String,
+      price: map["price"] as double,
+      type: types[map["type"] as int],
+      images: []
+    );
+  }
 }
