1836f9836985f3ef2199c108aabefe01e52e9e2d
M S <mo.sa963@hotmail.com>
2023-08-23 21:42:01 +0300

    Fix flex error when direction is vertical

--- lib/widgets/products_list/product_item.dart
+++ lib/widgets/products_list/product_item.dart
@@ -14,6 +14,7 @@ class ProductItem extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(5),
       height: direction == Axis.horizontal ? 130 : null,
+      width: direction == Axis.horizontal ? null : 130,
       child: Flex(
         direction: direction,
         children: [
@@ -23,32 +24,33 @@ class ProductItem extends StatelessWidget {
             width: direction == Axis.horizontal ? 10 : null,
           ),
           Expanded(
+              flex: direction == Axis.horizontal ? 1 : 0,
               child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(product.name,
-                  style: Theme.of(context).textTheme.headlineSmall),
-              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(product.price.toString(),
-                      textScaleFactor: 1.2,
-                      style: const TextStyle(color: Colors.green)),
-                  const Text("دولار")
+                  Text(product.name,
+                      style: Theme.of(context).textTheme.headlineSmall, overflow: TextOverflow.ellipsis),
+                  Row(
+                    children: [
+                      Text(product.price.toString(),
+                          textScaleFactor: 1.2,
+                          style: const TextStyle(color: Colors.green), overflow: TextOverflow.ellipsis),
+                      const Text("دولار")
+                    ],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(3),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(4)),
+                      color: Color.fromARGB(138, 203, 203, 203),
+                    ),
+                    child: Text(product.shop,
+                        style: const TextStyle(
+                            color: Color.fromARGB(255, 153, 153, 153)), overflow: TextOverflow.ellipsis),
+                  )
                 ],
-              ),
-              Container(
-                padding: const EdgeInsets.all(3),
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(4)),
-                  color: Color.fromARGB(138, 203, 203, 203),
-                ),
-                child: Text(product.shop,
-                    style: const TextStyle(
-                        color: Color.fromARGB(255, 153, 153, 153))),
-              )
-            ],
-          )),
+              )),
         ],
       ),
     );
